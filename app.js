f1b16246fd704208067ed5b27035a5d56f41444b
Jayakrishnan Jayabal <jayakrishnan.jayabal@acquia.com>
2017-10-05 10:19:19 +0530

    Nodejs with Mysql working version2.0.

--- app.js
+++ app.js
@@ -1,14 +1,95 @@
-var express = require('express');
-var session = require('cookie-session'); // Loads the piece of middleware for sessions
-var bodyParser = require('body-parser'); // Loads the piece of middleware for managing the settings
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+console.log("Nodejs running");
 
-var app = express();
+var http = require('http');
+var express     =    require('express');
+var formidable = require('formidable');
+var mysql = require('mysql');
+var uuid = require('uuid');
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to Nodejs CMS');
+var app  = express();
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "messageBox"
+});
+
+var response = {
+  status  : 200,
+  data : 'Updated Successfully'
+}
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to SQL DB!");
+
+});
+
+app.get('/',function(req,res){
+  res.sendFile("index.html");
+});
+
+app.post('/onRegister',function(req,res){
+
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, values) {
+
+    console.log(values.name);
+    var sqlinsert = "INSERT INTO inbox (id, title, message) VALUES (UUID(),'"+values.name+"','"+values.message+"');";
+
+    console.log('>>>>>>>>>'+sqlinsert);
+    con.query(sqlinsert, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      res.redirect("/view");
+    });
+
+  });
+
+});
+
+app.get('/ondelete', function(req, res) {
+  console.log(req.query.id);
+  var deletesql = "delete from inbox where id = '"+req.query.id+"'";
+
+  console.log('>>>>>>>>>'+deletesql);
+  con.query(deletesql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record deleted"+result);
+    res.redirect("/view");
+  })
+
+});
+
+app.get('/update', function(req,res){
+  console.log(req.query.name);
+
+  var updatesql= 'UPDATE inbox SET title="'+req.query.name+'",message="'+req.query.message+'" where id="'+req.query.id+'"';
+  //var updatesql = "UPDATE inbox SET title='"+req.query.name+"','message ='"+req.query.message+"where id ='"+req.query.id+"'";
+
+  console.log('>>>>>>>>>'+updatesql);
+  con.query(updatesql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record updated");
+    res.redirect("/view");
+  })
+
 });
 
 
-app.listen(8080);
+app.get('/load',function(req,res){
+
+  con.query("SELECT * FROM inbox", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    return res.send(result);
+  });
+});
+
+app.get('/view',function(req,res){
+  res.sendFile("view.html");
+});
+
+app.listen(8080,function(){
+  console.log("Working on port 8080");
+});
